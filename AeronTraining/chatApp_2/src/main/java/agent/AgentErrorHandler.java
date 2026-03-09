eea5368bf86b99419fab6cc31c3b1801b58afa75
Pol Curto <pol.curto@weareadaptive.com>
2026-03-09 16:03:55 +0100

    feat: Chat app task2 setup

---
+++ AeronTraining/chatApp_2/src/main/java/agent/AgentErrorHandler.java
@@ -0,0 +1,12 @@
+package agent;
+
+import org.agrona.ErrorHandler;
+
+public class AgentErrorHandler implements ErrorHandler
+{
+    @Override
+    public void onError(final Throwable throwable)
+    {
+        System.out.println("Error in agent: " + throwable.getMessage());
+    }
+}
---
+++ AeronTraining/chatApp_2/src/main/java/common/Globals.java
@@ -0,0 +1,7 @@
+package common;
+
+public class Globals
+{
+    public static int STREAM_ID = 10;
+    public static String AERON_DIR_PATH = "/Volumes/DevShm/aeron-training";
+}
