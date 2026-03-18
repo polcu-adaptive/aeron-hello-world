32e9f11c5a3f3434dea8694064ed414b973b26c6
Pol Curto <pol.curto@weareadaptive.com>
2026-03-18 16:45:52 +0100

    refactor: move logic from verticle to service

--- AeronTraining/chatServer_2/src/main/java/com/weareadaptive/chatServer/task2/web/WebGatewayService.java
+++ AeronTraining/chatServer_2/src/main/java/com/weareadaptive/chatServer/task2/web/WebGatewayService.java
@@ -1,13 +1,107 @@
 package com.weareadaptive.chatServer.task2.web;
 
+import io.vertx.core.http.WebSocket;
+import io.vertx.core.json.JsonObject;
+import org.agrona.MutableDirectBuffer;
+import org.agrona.concurrent.UnsafeBuffer;
 import org.agrona.concurrent.ringbuffer.OneToOneRingBuffer;
+import task3.src.main.resources.AeronMessageDecoder;
+import task3.src.main.resources.AeronMessageEncoder;
+import task3.src.main.resources.MessageHeaderDecoder;
+import task3.src.main.resources.MessageHeaderEncoder;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WebGatewayService
 {
-    //private final OneToOneRingBuffer;
+    private final OneToOneRingBuffer innerRingBuffer;
+    private final OneToOneRingBuffer outerRingBuffer;
 
-    public void registerWebSocket()
+    private final MessageHeaderEncoder headerEncoder = new MessageHeaderEncoder();
+    private final MessageHeaderDecoder headerDecoder = new MessageHeaderDecoder();
+    private final AeronMessageEncoder messageEncoder = new AeronMessageEncoder();
+    private final AeronMessageDecoder messageDecoder = new AeronMessageDecoder();
+
+    private final UnsafeBuffer unsafeBuffer = new UnsafeBuffer(ByteBuffer.allocateDirect(4096));
+
+    private final List<WebSocket> webSocketList = new ArrayList<>();
+
+    public WebGatewayService(final OneToOneRingBuffer innerRingBuffer, final OneToOneRingBuffer outerRingBuffer)
     {
+        this.innerRingBuffer = innerRingBuffer;
+        this.outerRingBuffer = outerRingBuffer;
+    }
 
+    public void registerWebSocket(final WebSocket newWebSocket)
+    {
+        System.out.println("Register web socket");
+        webSocketList.add(newWebSocket);
+
+        // On message event
+        newWebSocket.textMessageHandler(this::onWebSocketMessage);
+
+        // On close event
+        newWebSocket.closeHandler(v -> onWebSocketClose(newWebSocket));
+    }
+
+    public void pollMessages()
+    {
+        outerRingBuffer.read(this::decodeAndSendMessages);
+    }
+
+    private void onWebSocketMessage(final String message)
+    {
+        System.out.println("Websocket message received: " + message);
+
+        messageEncoder.wrapAndApplyHeader(unsafeBuffer, 0, headerEncoder);
+        messageEncoder.message(message);
+        messageEncoder.inputTimestamp(System.nanoTime());
+
+        final int length = headerEncoder.encodedLength() + messageEncoder.encodedLength();
+        innerRingBuffer.write(1, unsafeBuffer, 0, length);
+    }
+
+    private void onWebSocketClose(final WebSocket closingWebSocket)
+    {
+        webSocketList.remove(closingWebSocket);
+        System.out.println("Websocket has been closed");
+    }
+
+    private void decodeAndSendMessages(final int msgTypeId, final MutableDirectBuffer buffer, final int offset, final int length)
+    {
+        //Decode SBE message
+        headerDecoder.wrap(buffer, offset);
+
+        final int actingBlockLength = headerDecoder.blockLength();
+        final int actingVersion = headerDecoder.version();
+
+        final int totalOffset = headerDecoder.encodedLength() + offset;
+        messageDecoder.wrap(buffer, totalOffset, actingBlockLength, actingVersion);
+
+        final String message = messageDecoder.message();
+        final long netTimestamp = messageDecoder.netTimestamp();
+        final long inputTimestamp = messageDecoder.inputTimestamp();
+        final long serverTimestamp = messageDecoder.serverTimestamp();
+
+        // Compute latency
+        final double inputLatencyMs = (System.nanoTime() - inputTimestamp) / 1_000_000.0;
+        final double netLatencyMs = (System.nanoTime() - netTimestamp) / 1_000_000.0;
+        final double serverLatencyMs = (System.nanoTime() - serverTimestamp) / 1_000_000.0;
+
+        final JsonObject jsonObject = new JsonObject()
+                .put("Message", message)
+                .put("InputLatency", inputLatencyMs)
+                .put("NetLatency", netLatencyMs)
+                .put("ServerLatency", serverLatencyMs);
+
+        webSocketList.forEach(webSocket ->
+        {
+            if (!webSocket.isClosed())
+            {
+                webSocket.writeTextMessage(jsonObject.encode());
+            }
+        });
     }
 }
--- AeronTraining/chatServer_2/src/main/java/com/weareadaptive/chatServer/task2/web/WebGatewayVerticle.java
+++ AeronTraining/chatServer_2/src/main/java/com/weareadaptive/chatServer/task2/web/WebGatewayVerticle.java
@@ -3,41 +3,17 @@ package com.weareadaptive.chatServer.task2.web;
 import io.vertx.core.Future;
 import io.vertx.core.VerticleBase;
 import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.WebSocket;
-import io.vertx.core.json.JsonObject;
-import org.agrona.MutableDirectBuffer;
-import org.agrona.concurrent.UnsafeBuffer;
 import org.agrona.concurrent.ringbuffer.OneToOneRingBuffer;
-import task3.src.main.resources.AeronMessageDecoder;
-import task3.src.main.resources.AeronMessageEncoder;
-import task3.src.main.resources.MessageHeaderDecoder;
-import task3.src.main.resources.MessageHeaderEncoder;
-
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.List;
 
 public class WebGatewayVerticle extends VerticleBase
 {
     private final int configuredPort;
-
-    private final OneToOneRingBuffer innerRingBuffer;
-    private final OneToOneRingBuffer outerRingBuffer;
-
-    private final MessageHeaderEncoder headerEncoder = new MessageHeaderEncoder();
-    private final MessageHeaderDecoder headerDecoder = new MessageHeaderDecoder();
-    private final AeronMessageEncoder messageEncoder = new AeronMessageEncoder();
-    private final AeronMessageDecoder messageDecoder = new AeronMessageDecoder();
-
-    private final UnsafeBuffer unsafeBuffer = new UnsafeBuffer(ByteBuffer.allocateDirect(4096));
-
-    private final List<WebSocket> webSocketList = new ArrayList<>();
+    private final WebGatewayService webGatewayService;
 
     public WebGatewayVerticle(final int configuredPort, final OneToOneRingBuffer innerRingBuffer, final OneToOneRingBuffer outerRingBuffer)
     {
         this.configuredPort = configuredPort;
-        this.innerRingBuffer = innerRingBuffer;
-        this.outerRingBuffer = outerRingBuffer;
+        this.webGatewayService = new WebGatewayService(innerRingBuffer, outerRingBuffer);
     }
 
     @Override
@@ -45,73 +21,13 @@ public class WebGatewayVerticle extends VerticleBase
     {
         final HttpServer httpServer = vertx.createHttpServer();
 
-        httpServer.webSocketHandler(webSocket ->
-        {
-            System.out.println("Register web socket");
-            webSocketList.add(webSocket);
+        httpServer.webSocketHandler(webGatewayService::registerWebSocket);
 
-            // On message event
-            webSocket.textMessageHandler(message ->
-            {
-                System.out.println("Websocket message received: " + message);
-
-                messageEncoder.wrapAndApplyHeader(unsafeBuffer, 0, headerEncoder);
-                messageEncoder.message(message);
-                messageEncoder.inputTimestamp(System.nanoTime());
-
-                final int length = headerEncoder.encodedLength() + messageEncoder.encodedLength();
-                innerRingBuffer.write(1, unsafeBuffer, 0, length);
-            });
-
-            // On close event
-            webSocket.closeHandler(v ->
-            {
-                webSocketList.remove(webSocket);
-                System.out.println("Websocket has been closed");
-            });
-        });
-
-        vertx.setPeriodic(1, id -> outerRingBuffer.read(this::pollMessages));
+        vertx.setPeriodic(1, id -> webGatewayService.pollMessages());
 
         return httpServer
                 .listen(configuredPort)
                 .onSuccess(server -> System.out.println("HTTP server started on port " + server.actualPort()))
                 .onFailure(throwable -> System.out.println("HTTP server error"));
-    }
-
-    private void pollMessages(final int msgTypeId, final MutableDirectBuffer buffer, final int offset, final int length)
-    {
-        //Decode SBE message
-        headerDecoder.wrap(buffer, offset);
-
-        final int actingBlockLength = headerDecoder.blockLength();
-        final int actingVersion = headerDecoder.version();
-
-        final int totalOffset = headerDecoder.encodedLength() + offset;
-        messageDecoder.wrap(buffer, totalOffset, actingBlockLength, actingVersion);
-
-        final String message = messageDecoder.message();
-        final long netTimestamp = messageDecoder.netTimestamp();
-        final long inputTimestamp = messageDecoder.inputTimestamp();
-        final long serverTimestamp = messageDecoder.serverTimestamp();
-
-        // Compute latency
-        final double inputLatencyMs = (System.nanoTime() - inputTimestamp) / 1_000_000.0;
-        final double netLatencyMs = (System.nanoTime() - netTimestamp) / 1_000_000.0;
-        final double serverLatencyMs = (System.nanoTime() - serverTimestamp) / 1_000_000.0;
-
-        final JsonObject jsonObject = new JsonObject()
-                .put("Message", message)
-                .put("InputLatency", inputLatencyMs)
-                .put("NetLatency", netLatencyMs)
-                .put("ServerLatency", serverLatencyMs);
-
-        webSocketList.forEach(webSocket ->
-        {
-            if (!webSocket.isClosed())
-            {
-                webSocket.writeTextMessage(jsonObject.encode());
-            }
-        });
     }
 }
